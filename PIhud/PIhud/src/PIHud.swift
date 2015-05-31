8e09b8ab9a2d3def2a4905aba78a14c6430f692c
pixel-ink <fujiwara@yoshiki.mobi>
2015-05-31 20:36:39 +0900

    refactoring

--- PIhud/PIhud/src/PIHud.swift
+++ PIhud/PIhud/src/PIHud.swift
@@ -2,30 +2,20 @@
 
 import UIKit
 
-class PIHudConfig {
-  
-  var speed = 0.3
-  var delay = 1.5
-  var cornerRadius = CGFloat(15.0)
-  var backgroundColor = UIColor(white: 1.0, alpha: 0.6)
-  var tintColor = UIColor(white: 0.0, alpha: 0.6)
-  var hudSize = CGSize(width: 100, height: 100)
-  var font = UIFont.boldSystemFontOfSize(20)
-  private var fontHight = CGFloat(20.0)
-  
-  class var shared: PIHudConfig {
-    struct Static {
-      static let instance: PIHudConfig = PIHudConfig()
-    }
-    return Static.instance
+class PIHud {
+
+  class Config {
+    var speed = 0.3
+    var delay = 1.5
+    var cornerRadius = CGFloat(15.0)
+    var backgroundColor = UIColor(white: 1.0, alpha: 0.6)
+    var tintColor = UIColor(white: 0.0, alpha: 0.6)
+    var hudSize = CGSize(width: 100, height: 100)
+    var font = UIFont.boldSystemFontOfSize(20)
+    private var fontHight = CGFloat(20.0)
   }
   
-}
-
-
-
-class PIHud {
-  
+  private let config: Config
   private let target:UIView
   private let hud:UIView
   private let hudIcon:UIImageView
@@ -34,6 +24,7 @@ class PIHud {
   
   init(target:UIView) {
     self.target = target
+    config = Config()
     
     hud = UIView()
     hud.alpha = 0.0
@@ -45,8 +36,8 @@ class PIHud {
     
     hudMessage = UILabel()
     hudMessage.textAlignment = .Center
-    hudMessage.font = PIHudConfig.shared.font
-    hudMessage.textColor = PIHudConfig.shared.tintColor
+    hudMessage.font = config.font
+    hudMessage.textColor = config.tintColor
     hud.addSubview(hudMessage)
     
     toast = UILabel()
@@ -65,20 +56,20 @@ class PIHud {
     PIHudQueue.toast.add(op)
   }
   
-//  func progressStart(pict:UIImage, text:String) {
-//    let op = progressStartOperation(pict, text: text)
-//    PIHudQueue.hud.add(op)
-//  }
-//  
-//  func progressEnd() {
-//    let op = progressEndOperation()
-//    PIHudQueue.hud.add(op)
-//  }
+  func progressStart(pict:UIImage, text:String) {
+    let op = progressStartOperation(pict, text: text)
+    PIHudQueue.hud.add(op)
+  }
+  
+  func progressEnd() {
+    let op = progressEndOperation()
+    PIHudQueue.hud.add(op)
+  }
 
   private func hudLayout(withMessage:Bool) {
-    let size = PIHudConfig.shared.hudSize
+    let size = config.hudSize
     if withMessage {
-      let fh = PIHudConfig.shared.fontHight
+      let fh = config.fontHight
       hudMessage.hidden = false
       hudIcon.frame.size = CGSizeMake(size.w,size.h - fh)
       hudIcon.frame.origin = CGPointZero
@@ -95,16 +86,17 @@ class PIHud {
     return {
       [weak self] final in
       if let s = self {
-        s.hud.frame = PIHudImage.center(PIHudConfig.shared.hudSize, bound: s.target.size ?? CGSizeZero)
-        s.hud.backgroundColor = PIHudConfig.shared.backgroundColor
-        s.hud.layer.cornerRadius = PIHudConfig.shared.cornerRadius
+        s.hud.frame.size = s.config.hudSize
+        s.hud.center = s.target.center
+        s.hud.backgroundColor = s.config.backgroundColor
+        s.hud.layer.cornerRadius = s.config.cornerRadius
         s.hudIcon.image = pict
         s.hudMessage.text = text
         s.hudLayout(true)
-        PIHudAnime.start({
+        s.start({
           s.hud.alpha = 1.0
           }){
-            PIHudAnime.delay ({
+            s.delay ({
               s.hud.alpha = 0.0
               }){final()}
         }
@@ -114,59 +106,84 @@ class PIHud {
     }
   }
   
-//  func progressStartOperation(pict:UIImage, text:String ) -> ( (final:()->()) -> () ) {
-//    return {
-//      [weak self] final in
-//      if let s = self {
-//        s.hud.frame = PIHudImage.center(PIHudConfig.shared.hudSize, bound: s.target.size ?? CGSizeZero)
-//        s.hud.backgroundColor = PIHudConfig.shared.backgroundColor
-//        s.hud.image = PIHudImage.hudImage(pict, text: text)
-//        s.hud.layer.cornerRadius = PIHudConfig.shared.cornerRadius
-//        PIHudAnime.start({
-//          s.hud.alpha = 1.0
-//          }){final()}
-//      } else {
-//        final()
-//      }
-//    }
-//  }
-//
-//  func progressEndOperation() -> ( (final:()->()) -> () ) {
-//    return {
-//      [weak self] final in
-//      if let s = self {
-//        PIHudAnime.start({
-//          s.hud.alpha = 0.0
-//          }){final()}
-//      } else {
-//        final()
-//      }
-//    }
-//  }
+  func progressStartOperation(pict:UIImage, text:String ) -> ( (final:()->()) -> () ) {
+    return {
+      [weak self] final in
+      if let s = self {
+        s.hud.frame.size = s.config.hudSize
+        s.hud.center = s.target.center
+        s.hud.backgroundColor = s.config.backgroundColor
+        s.hud.layer.cornerRadius = s.config.cornerRadius
+        s.hudIcon.image = pict
+        s.hudMessage.text = text
+        s.hudLayout(true)
+        s.start({
+          s.hud.alpha = 1.0
+          }){final()}
+      } else {
+        final()
+      }
+    }
+  }
+
+  func progressEndOperation() -> ( (final:()->()) -> () ) {
+    return {
+      [weak self] final in
+      if let s = self {
+        s.start({
+          s.hud.alpha = 0.0
+          }){final()}
+      } else {
+        final()
+      }
+    }
+  }
   
   func toastOperation(text:String) -> ( (final:()->()) -> () ) {
     return {
       [weak self] final in
       if let s = self {
-        s.toast.backgroundColor = PIHudConfig.shared.backgroundColor
-        s.toast.font = PIHudConfig.shared.font
+        s.toast.backgroundColor = s.config.backgroundColor
+        s.toast.font = s.config.font
         s.toast.text = text
         s.toast.sizeToFit()
-        s.toast.textColor = PIHudConfig.shared.tintColor
+        s.toast.textColor = s.config.tintColor
         s.toast.textAlignment = .Center
         s.toast.frame.inset(dx: -10, dy: -10)
-        s.toast.layer.cornerRadius = PIHudConfig.shared.cornerRadius
+        s.toast.layer.cornerRadius = s.config.cornerRadius
         s.toast.clipsToBounds = true
         s.toast.center = s.target.center
         s.toast.frame.origin.y = s.target.h - s.toast.h - 30.0
-        PIHudAnime.start({
+        s.start({
           s.toast.alpha = 1.0
           }){
-            PIHudAnime.delay ({
+            s.delay ({
               s.toast.alpha = 0.0
               }){final()}
         }
       } else {final()}
     }
   }
+  
+  func start(animations:()->Void, completion:() -> Void) {
+    anime(false, animations: animations, completion: completion)
+  }
+  
+  func delay(animations:()->Void, completion:() -> Void) {
+    anime(true, animations: animations, completion: completion)
+  }
+
+  func anime(delay:Bool, animations:() -> Void, completion:() -> Void) {
+    UIView.animateWithDuration(
+      config.speed,
+      delay: delay ? config.delay : 0.0,
+      options: UIViewAnimationOptions.CurveEaseIn,
+      animations: animations,
+      completion: {
+        (fin:Bool) -> Void in
+        completion()
+      }
+    )
+  }
+  
 }
